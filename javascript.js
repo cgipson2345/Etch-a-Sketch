fc282739b57b9c95aebf72f9922e2930874c1ede
billblaster <102612368+billblaster@users.noreply.github.com>
2023-11-02 10:54:22 -0700

    added functionality to change amount of squares

--- javascript.js
+++ javascript.js
@@ -2,16 +2,39 @@
 //squareGrid.classList.add("squareGrid")
 //squareGrid.textContent = "doofenshmirtz"
 const  container = document.querySelector('.container');
+let Grids = document.querySelectorAll('.squareGrid');
+function makeGrids(squares) {
+    // @ts-ignore
+   
+    
+    container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
+    
+    for(let i = 0; i < (squares * squares); i++){
+        const squareGrid = document.createElement('div');
+        
+        squareGrid.id = i.toString();
+        squareGrid.classList.add("squareGrid")
+        // @ts-ignore
+        container.appendChild(squareGrid);
+    }
 
-for(let i = 0; i < (16 * 16); i++){
-    const squareGrid = document.createElement('div');
-    squareGrid.id = i.toString();
-    squareGrid.classList.add("squareGrid")
-    container.appendChild(squareGrid);
+    Grids = document.querySelectorAll('.squareGrid');
+    Grids.forEach((grid)=> 
+
+{
+    grid.addEventListener('mouseover',(e) => {
+        // @ts-ignore
+        e.target.style.backgroundColor = 'black';
+
+    });
+
+});
+
 }
 
-const Grids = document.querySelectorAll('.squareGrid');
+makeGrids(16);
 
+/*
 Grids.forEach((grid)=> 
 
 {
@@ -21,4 +44,23 @@ Grids.forEach((grid)=>
 
     });
 
-});
+});*/
+
+let Input = 0;
+const  Prompt = document.querySelector('#Prompt');
+// @ts-ignore
+Prompt.addEventListener('click', () => 
+{
+    Input = Number(prompt("Enter the number of squares you want on each side"));
+    while(Input > 100 || Input <= 0) {
+        Input = Number(prompt("Enter the number of squares you want on each side"));
+    }
+    // @ts-ignore
+    while(container.firstChild) {
+        container?.removeChild(container.lastChild);
+    }
+    makeGrids(Input);
+    // @ts-ignore
+    
+}
+);
